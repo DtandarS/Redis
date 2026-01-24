54adf37b1417823d747efb2435edf50d189b930d
DtandarS <tom15naksutti@gmail.com>
2026-01-24 07:25:12 +0200

    Update server.cpp
    buffer removing function that removes the initial ones to make more space for more request.

--- The-Redis/server.cpp
+++ The-Redis/server.cpp
@@ -104,9 +104,16 @@ struct Conn
 };
 /* ============================ */
 
+//  Appends to the back of the buffer array
 static void buf_append(std::vector<uint8_t> &buf, const uint8_t *data, size_t len)
 {
   buf.insert(buf.end(), data, data + len);
+}
+
+//  Remnoves from buffer front
+static void buf_consume(std::vector<uint8_t> &buf, size_t n)
+{
+  buf.erase(buf.begin(), buf.begin() + n);
 }
 
 static bool one_req(Conn *conn)
